e411dc7b7411c6f877e5ab7dd6ded2d2df40248b
Paul Krohn <paul@daemonize.com>
2015-06-14 09:04:43 -0700

    add handling options & positional arguments to sample script.

--- report.py
+++ report.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', dest="debug", action="store_true", default=False)
+# in our infinite wisdom, this is how we handle the default use case of
+# a list of positional arguments
+parser.add_argument('spam_files', metavar='FILE', type=str, nargs='+',
+                    help='a list of files')
+
+args = parser.parse_args()
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.WARNING)
 
 
 from spamcop.client import SpamCopClient
 
 
 client = SpamCopClient()
-# client._show_cookie_file()
+for spam_file in args.spam_files:
+    client.report(path_to_spam_mail_file=spam_file)
