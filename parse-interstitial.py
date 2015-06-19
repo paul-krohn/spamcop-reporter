0e11e53008a98e7d9fdb8d4aa4c8dae5d11c845d
Paul Krohn <paul@daemonize.com>
2015-06-19 05:35:09 -0700

    simplify interstitial parser.

--- parse-interstitial.py
+++ parse-interstitial.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 import sys
-import json
 
-from spamcop.response import SpamCopHtmlPFinder, SpamCopMetaFinder
+from spamcop.response import SpamCopFinder
+finder = SpamCopFinder()
 
-# p_finder = SpamCopHtmlPFinder(verbose=1)
-# p_finder.feed(open(sys.argv[1]).read())
-# print(json.dumps(p_finder.paragraphs, indent=4))
-meta_finder = SpamCopMetaFinder()
-meta_finder.feed(open(sys.argv[1]).read())
-# print(json.dumps(meta_finder.metas, indent=4))
-refresh = meta_finder.detect_meta_refresh()
-
+refresh = finder.meta_refresh_seconds(open(sys.argv[1]).read())
 print("the refresh is: %s" % refresh)
